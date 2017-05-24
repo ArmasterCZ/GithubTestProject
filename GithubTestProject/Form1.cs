1eab524c976237eb142061cb8a589835eefc0f1a
Armaster.notebook <Akrijfuadlaf@seznam.cz>
2017-05-24 12:10:08 +0200

    přepracováno na základě připomínek..

--- GithubTestProject/Form1.cs
+++ GithubTestProject/Form1.cs
@@ -20,7 +20,7 @@ namespace GithubTestProject
         private void button1_Click(object sender, EventArgs e)
         {
             // notebook
-            //
+            // comment1
             //
             //
         }
